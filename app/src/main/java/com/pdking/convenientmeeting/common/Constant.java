8c3c22d3af94491317fdca082a4ae2812f348675
PdKingLiu <931942280@qq.com>
2019-01-29 12:25:45 +0800

    测试

---
+++ app/src/main/java/com/pdking/convenientmeeting/common/Constant.java
@@ -0,0 +1,15 @@
+package com.pdking.convenientmeeting.common;
+
+/**
+ * @author liupeidong
+ * Created on 2019/1/29 12:21
+ */
+public class Constant {
+
+    /* SMS的AppID和key */
+    public static final String APP_ID_SMS = "VRD7r4RRdZvTK8D6NBn6WNXXYdLwRiTyPqDyrUfzwZ1";
+    public static final String SDK_KEY_SMS = "6BUzyssaSocX7i9ef5wuWpPrj5nXEV7QHp5aSma14qHb";
+
+
+    /* */
+}
