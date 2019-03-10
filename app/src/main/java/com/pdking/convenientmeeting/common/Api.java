256e88711271e99290abc39b61d0af77c5111698
PdKingLiu <931942280@qq.com>
2019-03-10 20:18:59 +0800

    记录、我 界面完成

---
+++ app/src/main/java/com/pdking/convenientmeeting/common/Api.java
@@ -0,0 +1,10 @@
+package com.pdking.convenientmeeting.common;
+
+/**
+ * @author liupeidong
+ * Created on 2019/3/10 20:18
+ */
+public class Api {
+
+    
+}
--- app/src/main/java/com/pdking/convenientmeeting/fragment/RecordFragment.java
+++ app/src/main/java/com/pdking/convenientmeeting/fragment/RecordFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +22,12 @@ import com.pdking.convenientmeeting.R;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
 
 /**
  * @author liupeidong
@@ -51,6 +58,14 @@ public class RecordFragment extends Fragment {
         return mView;
     }
 
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        super.onHiddenChanged(hidden);
+        if (!hidden) {
+            initChart();
+        }
+    }
+
     private void initChart() {
         List<PieEntry> pieCharts = new ArrayList<>();
         pieCharts.add(new PieEntry(10, "缺勤"));
@@ -60,7 +75,7 @@ public class RecordFragment extends Fragment {
         PieDataSet pieDataSet = new PieDataSet(pieCharts, "");
         PieData data = new PieData(pieDataSet);
         data.setValueTextColor(Color.WHITE);
-        data.setValueTextSize(16f);
+        data.setValueTextSize(14f);
         data.setDrawValues(true);
         data.setValueFormatter(new PercentFormatter());
         List<Integer> integerList = new ArrayList<>();
@@ -76,16 +91,16 @@ public class RecordFragment extends Fragment {
         pieChart.getLegend();
         pieChart.setRotationEnabled(false);
         pieChart.setHighlightPerTapEnabled(true);
-        pieChart.animateX(3000, Easing.EaseOutBounce);
+        pieChart.animateX(2000, Easing.EaseOutCirc);
         Description description = new Description();
         description.setText("");
         pieChart.setDescription(description);
         Legend l = pieChart.getLegend();
-        l.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
-        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
-        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setOrientation(Legend.LegendOrientation.VERTICAL);
         l.setDrawInside(false);
-        l.setXEntrySpace(20f);
+        l.setXEntrySpace(0);
         l.setYOffset(20);
         // 输入标签样式
         pieChart.setEntryLabelColor(Color.WHITE);
