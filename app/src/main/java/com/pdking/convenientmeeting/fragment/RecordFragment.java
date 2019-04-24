cecf378c374714348ea6b1962930ea8dde9ecc0b
PdKingLiu <931942280@qq.com>
2019-04-24 11:28:35 +0800

    加载PieChart

--- app/src/main/java/com/pdking/convenientmeeting/fragment/RecordFragment.java
+++ app/src/main/java/com/pdking/convenientmeeting/fragment/RecordFragment.java
@@ -1,5 +1,6 @@
 package com.pdking.convenientmeeting.fragment;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
@@ -14,6 +15,9 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.bigkoo.pickerview.builder.TimePickerBuilder;
+import com.bigkoo.pickerview.listener.OnTimeSelectListener;
+import com.bigkoo.pickerview.view.TimePickerView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
@@ -25,11 +29,22 @@ import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.google.gson.Gson;
 import com.pdking.convenientmeeting.R;
+import com.pdking.convenientmeeting.activity.BookRoomDetailActivity;
 import com.pdking.convenientmeeting.activity.ModificationUserDataActivity;
+import com.pdking.convenientmeeting.common.Api;
+import com.pdking.convenientmeeting.db.MeetingMessage;
+import com.pdking.convenientmeeting.db.MeetingMessageBean;
 import com.pdking.convenientmeeting.db.UserInfo;
+import com.pdking.convenientmeeting.db.UserToken;
+import com.pdking.convenientmeeting.utils.LoginCallBack;
+import com.pdking.convenientmeeting.utils.LoginStatusUtils;
 import com.pdking.convenientmeeting.utils.OkHttpUtils;
 import com.pdking.convenientmeeting.weight.TitleView;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 
 import org.litepal.LitePal;
 
@@ -37,7 +52,13 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -75,11 +96,38 @@ public class RecordFragment extends Fragment implements View.OnClickListener {
 
     private LinearLayout llSwitchoverChart;
 
+    private LinearLayout llYear;
+
     private TextView tvSwitchoverChart;
+
+    private TextView tvYear;
+
+    private TextView tvProportion;
+
+    private SmartRefreshLayout smartRefreshLayout;
 
     private static RecordFragment INSTANCE = null;
 
     private boolean chartTimeFlag = true;
+
+    private Calendar calendar = Calendar.getInstance();
+
+    private UserToken userToken;
+
+    private List<MeetingMessage> beanList;
+
+    private List<MeetingMessage> yearList;
+
+    private Date date = new Date();
+
+    private int year;
+
+    private float[] chartStatus = {10, 20, 30, 40};
+    private float[] chartIdentity = {0, 0};
+
+    @SuppressLint("SimpleDateFormat")
+    SimpleDateFormat format = new
+            SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     public static RecordFragment getINSTANCE() {
         if (INSTANCE == null) {
@@ -92,9 +140,11 @@ public class RecordFragment extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        Log.d("Lpp", "onCreateView: ");
         mView = inflater.inflate(R.layout.layout_recordfragment, container, false);
         init();
         initView(mView);
+        pieChart.setVisibility(View.INVISIBLE);
         initChart();
         return mView;
     }
@@ -122,10 +172,14 @@ public class RecordFragment extends Fragment implements View.OnClickListener {
 
     private void initChart() {
         List<PieEntry> pieCharts = new ArrayList<>();
-        pieCharts.add(new PieEntry(10, "缺勤"));
-        pieCharts.add(new PieEntry(20, "请假"));
-        pieCharts.add(new PieEntry(30, "迟到"));
-        pieCharts.add(new PieEntry(40, "正常"));
+        PieEntry pieEntry1 = new PieEntry(chartStatus[0], "缺勤");
+        PieEntry pieEntry2 = new PieEntry(chartStatus[1], "请假");
+        PieEntry pieEntry3 = new PieEntry(chartStatus[2], "迟到");
+        PieEntry pieEntry4 = new PieEntry(chartStatus[3], "正常");
+        pieCharts.add(pieEntry1);
+        pieCharts.add(pieEntry2);
+        pieCharts.add(pieEntry3);
+        pieCharts.add(pieEntry4);
         PieDataSet pieDataSet = new PieDataSet(pieCharts, "");
         PieData data = new PieData(pieDataSet);
         data.setValueTextColor(Color.WHITE);
@@ -150,8 +204,8 @@ public class RecordFragment extends Fragment implements View.OnClickListener {
         description.setText("");
         pieChart.setDescription(description);
         Legend l = pieChart.getLegend();
-        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
         l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
         l.setOrientation(Legend.LegendOrientation.VERTICAL);
         l.setDrawInside(false);
         l.setXEntrySpace(0);
@@ -167,7 +221,12 @@ public class RecordFragment extends Fragment implements View.OnClickListener {
         llUserData = view.findViewById(R.id.ll_user_data);
         llSwitchoverChart = view.findViewById(R.id.ll_switchover_chart);
         tvSwitchoverChart = view.findViewById(R.id.tv_switchover_chart);
+        llYear = view.findViewById(R.id.ll_year);
+        tvYear = view.findViewById(R.id.tv_year);
+        tvProportion = view.findViewById(R.id.tv_proportion);
+        smartRefreshLayout = view.findViewById(R.id.srl_flush);
         llSwitchoverChart.setOnClickListener(this);
+        llYear.setOnClickListener(this);
         llUserData.setOnClickListener(this);
     }
 
@@ -208,27 +267,139 @@ public class RecordFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        Log.d("Lpp", "onActivityCreated: ");
         super.onActivityCreated(savedInstanceState);
+        beanList = new ArrayList<>();
+        yearList = new ArrayList<>();
+        userInfo = LitePal.findAll(UserInfo.class).get(0);
+        userToken = LitePal.findAll(UserToken.class).get(0);
+        smartRefreshLayout.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh(@NonNull RefreshLayout refreshLayout) {
+                refreshData();
+            }
+        });
+        smartRefreshLayout.autoRefresh();
+//        refreshData();
         loadUserData();
+    }
+
+    private void refreshData() {
+        FormBody.Builder body = new FormBody.Builder()
+                .add("token", userToken.getToken())
+                .add(Api.RequestUserMeetingListBody[0], userInfo.getUserId() + "")
+                .add(Api.RequestUserMeetingListBody[1], 2 + "");
+        Request request = new Request.Builder()
+                .post(body.build())
+                .header(Api.RequestUserMeetingListHeader[0], Api.RequestUserMeetingListHeader[1])
+                .addHeader("token", userToken.getToken())
+                .url(Api.RequestUserMeetingListApi)
+                .build();
+        OkHttpUtils.requestHelper(request, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                smartRefreshLayout.finishRefresh(false);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String msg = response.body().string();
+                if (msg.contains("token过期!")) {
+                    LoginStatusUtils.stateFailure(getActivity(), new LoginCallBack() {
+                        @Override
+                        public void newMessageCallBack(UserInfo newInfo, UserToken newToken) {
+                            userInfo = newInfo;
+                            userToken = newToken;
+                        }
+                    });
+                    return;
+                }
+                MeetingMessageBean bean = new Gson().fromJson(msg, MeetingMessageBean.class);
+                if (bean != null && bean.status == 0) {
+                    for (MeetingMessage message : bean.data) {
+                        message.meetingType = 1;
+                    }
+                    smartRefreshLayout.finishRefresh(true);
+                    beanList.clear();
+                    beanList.addAll(bean.data);
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            changeUIByDataAndDate();
+                        }
+                    });
+                } else {
+                    smartRefreshLayout.finishRefresh(false);
+                }
+            }
+        });
+
+    }
+
+    private void changeUIByDataAndDate() {
+        yearList.clear();
+        calendar.setTime(date);
+        int year = calendar.get(Calendar.YEAR);
+        Date dateTem;
+        try {
+            for (int i = 0; i < beanList.size(); i++) {
+                dateTem = format.parse(beanList.get(i).startTime);
+                calendar.setTime(dateTem);
+                if (calendar.get(Calendar.YEAR) == year) {
+                    yearList.add(beanList.get(i));
+                }
+            }
+        } catch (Exception e) {
+
+        }
+        int normal = 0;
+        int absenteeism = 0;
+        int late = 0;
+        int leave = 0;
+        int total = yearList.size();
+        int master = 0;
+        int parter = 0;
+        for (int i = 0; i < yearList.size(); i++) {
+            if (yearList.get(i).userStatus == 1) {
+                normal++;
+            } else if (yearList.get(i).userStatus == 2) {
+                absenteeism++;
+            } else if (yearList.get(i).userStatus == 3) {
+                late++;
+            } else if (yearList.get(i).userStatus == 4) {
+                leave++;
+            }
+            if (yearList.get(i).masterId == userInfo.userId) {
+                master++;
+            } else {
+                parter++;
+            }
+        }
+        chartStatus[0] = (float) ((((absenteeism * 1.0) / total) * 1000) / 10.0);
+        chartStatus[1] = (float) ((((leave * 1.0) / total) * 1000) / 10.0);
+        chartStatus[2] = (float) ((((late * 1.0) / total) * 1000) / 10.0);
+        chartStatus[3] = (float) ((((normal * 1.0) / total) * 1000) / 10.0);
+        chartIdentity[0] = (float) ((((master * 1.0) / master + parter) * 1000) / 10.0);
+        chartIdentity[1] = (float) ((((parter * 1.0) / master + parter) * 1000) / 10.0);
+        tvProportion.setText(normal + " / " + total);
+        pieChart.setVisibility(View.VISIBLE);
+        initChart();
     }
 
     private void loadUserData() {
         if (iconFile.exists()) {
-            Log.d("Lpp", "civUserIcon:file ");
             Glide.with(this)
                     .load(iconFile)
                     .apply(new RequestOptions().diskCacheStrategy(DiskCacheStrategy
                             .NONE).skipMemoryCache(true))
                     .into(civUserIcon);
         } else {
-            Log.d("Lpp", "civUserIcon:avatarUrl ");
             Request request = new Request.Builder()
                     .url(userInfo.avatarUrl)
                     .build();
             OkHttpUtils.requestHelper(request, new Callback() {
                 @Override
                 public void onFailure(Call call, IOException e) {
-                    Log.d("Lpp", "onFailure: " + e.getMessage());
                 }
 
                 @Override
@@ -236,17 +407,12 @@ public class RecordFragment extends Fragment implements View.OnClickListener {
                     InputStream inputStream = null;
                     byte[] bytes = new byte[1024];
                     FileOutputStream fileOutputStream = null;
-                    long current = 0;
                     int len;
                     try {
-                        long total = response.body().contentLength();
-                        Log.d("Lpp", "onResponse: " + total);
                         inputStream = response.body().byteStream();
                         fileOutputStream = new FileOutputStream(iconFile);
                         while ((len = inputStream.read(bytes)) != -1) {
-                            current += len;
                             fileOutputStream.write(bytes, 0, len);
-                            Log.d("Lpp", "onResponse: " + current);
                         }
                         fileOutputStream.flush();
                         inputStream.close();
@@ -257,7 +423,6 @@ public class RecordFragment extends Fragment implements View.OnClickListener {
                                         .NONE).skipMemoryCache(true))
                                 .into(civUserIcon);
                     } catch (Exception e) {
-                        Log.d("Lpp", "onResponse: " + e.getMessage());
                     }
                 }
             });
@@ -282,13 +447,34 @@ public class RecordFragment extends Fragment implements View.OnClickListener {
                     chartTimeFlag = true;
                 }
                 break;
+            case R.id.ll_year:
+                chooseYear();
+                break;
         }
+    }
+
+    private void chooseYear() {
+        TimePickerView pvTime = new TimePickerBuilder(getActivity(), new OnTimeSelectListener() {
+            @Override
+            public void onTimeSelect(Date date, View v) {
+                queryData(date);
+            }
+        }).setType(new boolean[]{true, false, false, false, false, false})
+                .setTitleText("注：")
+                .build();
+        pvTime.setDate(Calendar.getInstance());
+        pvTime.show();
+    }
+
+    private void queryData(Date date) {
+        calendar.setTime(date);
+        tvYear.setText(calendar.get(Calendar.YEAR) + "年");
     }
 
     private void initIdentityChart() {
         List<PieEntry> pieCharts = new ArrayList<>();
-        pieCharts.add(new PieEntry(33.3f, "组织者"));
-        pieCharts.add(new PieEntry(66.6f, "参与者"));
+        pieCharts.add(new PieEntry(chartIdentity[0], "组织者"));
+        pieCharts.add(new PieEntry(chartIdentity[1], "参与者"));
         PieDataSet pieDataSet = new PieDataSet(pieCharts, "");
         PieData data = new PieData(pieDataSet);
         data.setValueTextColor(Color.WHITE);
